8910691d1b612ba4430fd2185e12beec00256c05
Kamal Walia <kamalwalia.dev@gmail.com>
2024-12-13 11:41:52 +0530

    Updated font size

--- AddTextToImage.py
+++ AddTextToImage.py
@@ -10,9 +10,10 @@ img = Image.open('test.png')
 I1 = ImageDraw.Draw(img)
  
 # Add Text to an image
-font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 50)
+font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 60)
 # Name
-I1.text((330, 1008), "Kamal Walia", font=font, fill=(0, 0, 0))
+I1.text((350, 1000), "Kamal Walia", font=font, fill=(0, 0, 0))
+font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 50)
 # CVV
 I1.text((1600, 540), "1001", font=font, fill=(0, 0, 0)) 
 # Member Since
